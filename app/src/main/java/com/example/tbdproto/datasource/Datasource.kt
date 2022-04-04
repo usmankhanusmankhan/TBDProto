aee3902f550379ce3e182514e85fd089df5335fb
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-03 23:23:33 -0500

    finished recycler view scroll

--- app/src/main/java/com/example/tbdproto/datasource/Datasource.kt
+++ app/src/main/java/com/example/tbdproto/datasource/Datasource.kt
@@ -7,9 +7,9 @@ class Datasource {
 
     fun loadCardTitles(): List<MainCardTitles> {
         return listOf<MainCardTitles>(
-            MainCardTitles(R.string.start_run),
-            MainCardTitles(R.string.random_run),
-            MainCardTitles(R.string.last_run),
+            MainCardTitles(R.string.start_run, R.drawable.start_run),
+            MainCardTitles(R.string.random_run, R.drawable.popular_runs),
+            MainCardTitles(R.string.last_run, R.drawable.recent_runs),
         )
     }
 }
--- app/src/main/java/com/example/tbdproto/model/MainCardTitles.kt
+++ app/src/main/java/com/example/tbdproto/model/MainCardTitles.kt
@@ -1,3 +1,9 @@
 package com.example.tbdproto.model
 
-data class MainCardTitles(val stringResourceId: Int)
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class MainCardTitles(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
