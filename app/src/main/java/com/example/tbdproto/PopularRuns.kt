6b72fc6642b738f1e1d6388a6c03596a0c771d33
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-03 21:34:57 -0500

    screen set up

---
+++ app/src/main/java/com/example/tbdproto/PopularRuns.kt
@@ -0,0 +1,11 @@
+package com.example.tbdproto
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PopularRuns : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.running_map)
+    }
+}
