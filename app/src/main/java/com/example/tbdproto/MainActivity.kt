df6d9c801a3d220fdefde2243ff48e586b73cf44
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-03 22:57:01 -0500

    New images for recycler view

--- app/src/main/java/com/example/tbdproto/MainActivity.kt
+++ app/src/main/java/com/example/tbdproto/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.tbdproto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tbdproto.adapter.ItemAdapter
+import com.example.tbdproto.datasource.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadCardTitles()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
     }
 }
