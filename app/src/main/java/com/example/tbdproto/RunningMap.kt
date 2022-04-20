061c0d9b1581d6d17304efa4a11041e10a0c8004
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-20 05:33:57 -0500

    tracking implemented

--- app/src/main/java/com/example/tbdproto/RunningMap.kt
+++ app/src/main/java/com/example/tbdproto/RunningMap.kt
@@ -1,23 +1,35 @@
 package com.example.tbdproto
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.tbdproto.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.tbdproto.services.TrackingService
 import com.google.android.gms.maps.GoogleMap
 import kotlinx.android.synthetic.main.running_map.*
 
 class RunningMap : AppCompatActivity() {
     private var map: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         mapView.onCreate(savedInstanceState)
         setContentView(R.layout.running_map)
-
+        btnToggleRun.setOnClickListener {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
         mapView.getMapAsync {
             map = it
         }
 
     }
+
+    private fun sendCommandToService(action: String) =
+        Intent(this, TrackingService::class.java).also {
+            it.action = action
+           this.startService(it)
+        }
 
     override fun onResume() {
         super.onResume()
--- app/src/main/java/com/example/tbdproto/services/TrackingService.kt
+++ app/src/main/java/com/example/tbdproto/services/TrackingService.kt
@@ -1,6 +1,123 @@
 package com.example.tbdproto.services
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.location.Location
+import android.os.Looper
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.example.tbdproto.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.tbdproto.Constants.ACTION_PAUSE_SERVICE
+import com.example.tbdproto.Constants.ACTION_STOP_SERVICE
+import com.example.tbdproto.Constants.FASTEST_LOCATION_INTERVAL
+import com.example.tbdproto.Constants.LOCATION_UPDATE_INTERVAL
+import com.example.tbdproto.TrackingUtility
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
+import timber.log.Timber
+
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
+
 
 class TrackingService : LifecycleService() {
+
+    var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let {
+            when(it.action) {
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    Timber.d("Started or resumed service")
+                }
+                ACTION_PAUSE_SERVICE -> {
+                    Timber.d("Paused service")
+                }
+                ACTION_STOP_SERVICE -> {
+                    Timber.d("Stopped service")
+                }
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if(isTracking) {
+            if(TrackingUtility.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if(isTracking.value!!) {
+                result?.locations?.let { locations ->
+                    for(location in locations) {
+                        addPathPoint(location)
+                        Timber.d("NEW LOCATION: ${location.latitude}, ${location.longitude}")
+                    }
+                }
+            }
+        }
+    }
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
+
 }
