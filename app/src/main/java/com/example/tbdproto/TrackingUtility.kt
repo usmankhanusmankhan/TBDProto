86655bde0028e1cd98e5aab925c4e1b41b8af3bc
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-20 01:20:45 -0500

    figured out dependency stuff

---
+++ app/src/main/java/com/example/tbdproto/TrackingUtility.kt
@@ -0,0 +1,12 @@
+package com.example.tbdproto
+
+import android.Manifest
+import android.content.Context
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+
+object TrackingUtility {
+    fun hasLocationPermissions (context: Context) {
+        if(Build.VERSION.SDK_INT < )
+    }
+}
