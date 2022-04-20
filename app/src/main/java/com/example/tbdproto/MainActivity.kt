655e35421d2abb6cda0a7e5110dc15199d642560
usmankhanusmankhan <usman.khan@gmail.com>
2022-04-20 03:21:15 -0500

    enabled location services and tracking

--- app/src/main/java/com/example/tbdproto/MainActivity.kt
+++ app/src/main/java/com/example/tbdproto/MainActivity.kt
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tbdproto.adapter.ItemAdapter
-import com.example.tbdproto.databinding.ActivityMainBinding
 import com.example.tbdproto.datasource.Datasource
+import android.os.Build
+import pub.devrel.easypermissions.EasyPermissions
+import android.Manifest
+import pub.devrel.easypermissions.AppSettingsDialog
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestPermission()
         setContentView(R.layout.activity_main)
 
         val myDataset = Datasource().loadCardTitles()
@@ -19,6 +23,50 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
+    }
 
+    private fun requestPermission() {
+        if(Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+            return
+        }
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "Accept location permissions to use app",
+                0,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "Accept location permissions to use app",
+                0,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this,perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermission()
+        }
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {}
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 }
--- app/src/main/java/com/example/tbdproto/TrackingUtility.kt
+++ app/src/main/java/com/example/tbdproto/TrackingUtility.kt
@@ -6,7 +6,19 @@ import android.os.Build
 import pub.devrel.easypermissions.EasyPermissions
 
 object TrackingUtility {
-    fun hasLocationPermissions (context: Context) {
-        if(Build.VERSION.SDK_INT < )
+    fun hasLocationPermissions(context: Context) =
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        } else {
+            EasyPermissions.hasPermissions(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
     }
-}
